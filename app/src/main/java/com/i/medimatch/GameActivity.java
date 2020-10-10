c89e3d52f1adbabb6bc6c336922f70ff044cecfe
Agata <polejowska99agata@gmail.com>
2020-10-10 09:06:05 +0200

    Commit

--- app/src/main/java/com/i/medimatch/GameActivity.java
+++ app/src/main/java/com/i/medimatch/GameActivity.java
@@ -5,42 +5,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 
-public class GameActivity extends Fragment {
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
+public class GameActivity extends AppCompatActivity {
 
     public GameActivity() {
         // Empty constructor
     }
 
-    public static GameActivity newInstance(String param1, String param2) {
-        GameActivity fragment = new GameActivity();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    /* private static final String ARG_PARAM1 = "param1";
+     private static final String ARG_PARAM2 = "param2";
 
+     private String mParam1;
+     private String mParam2;
+
+     public static GameActivity newInstance(String param1, String param2) {
+         GameActivity fragment = new GameActivity();
+         Bundle args = new Bundle();
+         args.putString(ARG_PARAM1, param1);
+         args.putString(ARG_PARAM2, param2);
+         fragment.setArguments(args);
+         return fragment;
+     }
+ */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+  /*      if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        } */
+        setContentView(R.layout.fragment_game_activity);
     }
-
+/*
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_game_activity, container, false);
     }
+*/
+
 
 }
