01448f2e502ff28acb8eca8a4953ce770fb73fc0
Agata <polejowska99agata@gmail.com>
2020-10-17 12:15:11 +0200

    Made changes

--- app/src/main/java/com/i/medimatch/SettingsActivity.java
+++ app/src/main/java/com/i/medimatch/SettingsActivity.java
@@ -26,25 +26,25 @@ public class SettingsActivity extends AppCompatActivity {
         CheckBox med2 = findViewById(R.id.med2);
         CheckBox med3 = findViewById(R.id.med3);
         CheckBox med4 = findViewById(R.id.med4);
-        CheckBox med5 = findViewById(R.id.med5);
+
 
         medNames.add(new MedicationCard("AXTIL", true));
         medNames.add(new MedicationCard("RIDLIP", false));
         medNames.add(new MedicationCard("MED3", false));
         medNames.add(new MedicationCard("MED4", false));
-        medNames.add(new MedicationCard("MED5", false));
+
 
         med1.setText((medNames.get(0)).getName());
         med2.setText((medNames.get(1)).getName());
         med3.setText((medNames.get(2)).getName());
         med4.setText((medNames.get(3)).getName());
-        med5.setText((medNames.get(4)).getName());
+
 
         med1.setChecked((medNames.get(0)).isChecked());
         med2.setChecked(medNames.get(1).isChecked());
         med3.setChecked(medNames.get(2).isChecked());
         med4.setChecked(medNames.get(3).isChecked());
-        med5.setChecked(medNames.get(4).isChecked());
+
     }
 
 
@@ -55,13 +55,13 @@ public class SettingsActivity extends AppCompatActivity {
         CheckBox med2 = findViewById(R.id.med2);
         CheckBox med3 = findViewById(R.id.med3);
         CheckBox med4 = findViewById(R.id.med4);
-        CheckBox med5 = findViewById(R.id.med5);
+
 
         if (med1.isChecked()) { medCheckedNames.add(new MedicationCard ("AXTIL", true)); }
         if (med2.isChecked()) { medCheckedNames.add(new MedicationCard ("MED2", true)); }
         if (med3.isChecked()) { medCheckedNames.add(new MedicationCard ("MED3", true)); }
         if (med4.isChecked()) { medCheckedNames.add(new MedicationCard ("MED4", true)); }
-        if (med5.isChecked()) { medCheckedNames.add(new MedicationCard ("MED5", true)); }
+
 
         // Testing
         StringBuilder builder = new StringBuilder();
